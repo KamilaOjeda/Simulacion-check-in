8ebed0f82e704c0022fc2b965ec9163fb9d2f403
Kamila Ojeda <beau.ojeda@gmail.com>
2023-05-10 10:50:09 -0300

    Establece primera conexión con la base de datos.

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+from mysql.connector import errorcode
+
+app = FastAPI()
+
+# Configuración CORS, para permitir el acceso desde diferentes orígenes 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/seat")
+async def get_flights():
+    try:
+        connection = mysql.connector.connect(user = "bsale_test", password = "bsale_test", host = "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",  database = "airline", port = "3306")
+        print("Conexión exitosa a la base de datos")
+    except mysql.connector.Error as e:
+        print("Error al conectarse a la base de datos: {}".format(e))
+
+    cursor = connection.cursor()
+    connection.autoreconnect = True
+    connection.pool_size = 5
+
+    query = "SELECT * FROM seat"
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    
+    return {"seat": results}
+
